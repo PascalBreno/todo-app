c137076e441ec8c2f215ff9d91b3661407438709
PascalBreno <brenodml@live.com>
2019-10-11 00:27:11 -0300

    Cria módulo de Rotas para o menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,13 +2,11 @@ import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
 
 import React from "react";
-import Todo from "../main/todo/todo";
-import About from "../main/about/about";
 import Menu from "../template/menu";
+import Routes from "../main/routes";
 export default props => (
   <div className="container">
     <Menu></Menu>
-    <Todo></Todo>
-    <About></About>
+    <Routes />
   </div>
 );
