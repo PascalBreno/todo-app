e400257bd3b2b1840c87a6d49f1b006769adb29b
PascalBreno <brenodml@live.com>
2019-10-11 12:17:53 -0300

    Adiciona componentes Form Lista

--- frontend/src/main/todo/todo.jsx
+++ frontend/src/main/todo/todo.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import Header from "../../template/pageHeader";
+import Form from "../todo/todoform";
+import Lista from "../todo/todolista";
 export default class Todo extends Component {
   render() {
-    return <Header name="Tarefas" small="Cadastro"></Header>;
+    return (
+      <div>
+        <Header name="Tarefas" small="Cadastro"></Header>
+        <Form></Form>
+        <Lista></Lista>
+      </div>
+    );
   }
 }
