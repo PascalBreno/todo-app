1ca2b709f3a00dd652f51808da69740765a1e5c4
PascalBreno <brenodml@live.com>
2019-10-14 15:51:26 -0300

    Adiciona lógica de pesquisa e botão adicionar

--- frontend/src/main/todo/todo.jsx
+++ frontend/src/main/todo/todo.jsx
@@ -3,11 +3,31 @@ import Header from "../../template/pageHeader";
 import Form from "../todo/todoform";
 import Lista from "../todo/todolista";
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: "", list: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    console.log(this.state.description);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value });
+    console.log(e.target.value);
+  }
+
   render() {
     return (
       <div>
         <Header name="Tarefas" small="Cadastro"></Header>
-        <Form></Form>
+        <Form
+          handleChange={this.handleChange}
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+        ></Form>
         <Lista></Lista>
       </div>
     );
--- frontend/src/main/todo/todoform.jsx
+++ frontend/src/main/todo/todoform.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-
+import Grid from "../../template/grid";
+import IconButton from "../../template/iconButton";
 export default props => (
   <div role="form" className="todoForm">
-    <div className="col-xs-12 col-sm-9 col-md-10">
+    <Grid cols="12 9 10 10">
       <input
         id="description"
         className="form-control"
         placeholder="adiciona uma tarefa"
+        onChange={props.handleChange}
+        value={props.description}
       ></input>
-    </div>
-    <div className="col-xs-12 col-sm-3 col-md-2">
-      <button id="add" className="btn btn-primary">
-        <i className="fa fa-plus"></i>
-      </button>
-    </div>
+    </Grid>
+    <Grid cols="12 3 2 2">
+      <IconButton
+        btn="primary"
+        icon="plus"
+        hide={false}
+        onClick={props.handleAdd}
+      ></IconButton>
+    </Grid>
   </div>
 );
